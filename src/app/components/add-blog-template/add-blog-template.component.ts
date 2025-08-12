b67a7232a55eb2cf73e2e96d040c56f3f87a1c67
ea234 <anyang@gmx.de>
2025-08-12 09:10:37 +0200

    values in back ts class

--- src/app/components/add-blog-template/add-blog-template.component.ts
+++ src/app/components/add-blog-template/add-blog-template.component.ts
@@ -12,7 +12,19 @@ export class AddBlogTemplateComponent {
 
   ngSubmitMyForm( userForm : NgForm ) {
 
+    let my_form = userForm.form.value;
+
     console.log( 'ngSubmitMyForm' , userForm );
+
+    console.log( 'ngSubmitMyForm ' , my_form );
+
+    console.log( 'ngSubmitMyForm blog_date   =>', my_form.blog_date );
+    console.log( 'ngSubmitMyForm blog_header =>', my_form.blog_header );
+    console.log( 'ngSubmitMyForm blog_id     =>', my_form.blog_id );
+    console.log( 'ngSubmitMyForm blog_text   =>', my_form.blog_text );
+    console.log( 'ngSubmitMyForm blog_user   =>', my_form.blog_user );
+
+
 
   }
 
